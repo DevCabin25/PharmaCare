0aa18ee2709eab578e0ce2721d9bce941c4f46ae
mubashirbappi <mubashirbappi@gmail.com>
2025-07-28 17:03:30 +0600

    update Home screen added a loader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,27 @@
-import Image from "next/image";
-import under from "../../public/242462-P3LYBQ-600.jpg"
+'use client';
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/auth/login');
+  }, [router]);
+
   return (
-   <div className="">
-    <Image src={under} alt="under" ></Image>
+    <div className="flex items-center justify-center h-screen bg-white">
+      <div className="flex flex-col items-center gap-4">
+        {/* Pill Spinner */}
+        <div className="relative w-12 h-12 animate-spin rounded-full border-t-4 border-blue-500 border-solid">
+          <div className="absolute inset-0 flex items-center justify-center">
+            💊
+          </div>
+        </div>
 
-
-   </div>
+       
+      </div>
+    </div>
   );
 }
